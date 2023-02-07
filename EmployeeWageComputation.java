5e9170903698ef98c3f7fc5a932d9a4366dda88c
shital gawade <shitalgawade131@gmail.com>
2023-02-07 23:22:57 +0530

    wage

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -3,7 +3,8 @@ package emp.com;
 import java.util.Random;
 public class EmployeeWageComputation
 {
-    int Attend1;
+    int Attend1,Attend2;
+    int EmpRatePerHr,EmpHrs,w;
     EmployeeWageComputation()
     {
         Random r = new Random();
@@ -20,10 +21,25 @@ public class EmployeeWageComputation
             System.out.println("Employee is absent");
         }
     }
+    void wage()
+    {
+        if (Attend1 == 1)
+        {
+            EmpRatePerHr = 20;
+            EmpHrs = 8;
+            w = EmpRatePerHr * EmpHrs;
+            System.out.println("FullTime employee and Employee Wage is :-" + w);
+        }
+        else if (Attend1 == 0)
+        {
+            System.out.println("Employee Wage is :-0");
+        }
+    }
     public static void main(String[] args)
     {
         System.out.println("Welcome to Employee Wage Computation Program");
         EmployeeWageComputation e = new EmployeeWageComputation();
         e.attendance();
+        e.wage();
     }
 }
